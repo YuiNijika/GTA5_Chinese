ca6898941edb85dd3b9c69d38e5d838a9700301d
Katock-Cricket <787288968@qq.com>
2025-08-02 22:46:38 +0800

    修复md渲染

--- src/components/Tab3.vue
+++ src/components/Tab3.vue
@@ -1,17 +1,14 @@
 <script setup>
 import {ref, onMounted} from 'vue'
 import MarkdownIt from 'markdown-it'
+import 'github-markdown-css';
 
 const htmlContent = ref('<p>加载中...</p>')
-
+const md = new MarkdownIt();
 
 onMounted(async () => {
   try {
     const response = await fetch('/api/manual.md')
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    let md = new MarkdownIt();
     let text = await response.text();
     htmlContent.value = md.render(text);
   } catch (error) {
@@ -26,5 +23,6 @@ onMounted(async () => {
 <template>
   <input type="radio" name="my_tabs_2" class="tab" aria-label="说明"/>
 
-  <div class="tab-content border-base-300 bg-base-100 p-10" v-html="htmlContent"></div>
+  <div class="tab-content markdown-body border-base-300 bg-base-100 p-10" v-html="htmlContent">
+  </div>
 </template>
